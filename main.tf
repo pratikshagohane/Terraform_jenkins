eae039b0e59ddc20483e572ebdaf62579c06f8cc
pratikshagohane <pratikshagohane7@gmail.com>
2026-08-27 19:48:05 +0530

    Add AWS provider and resources also

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "ap-south-1"
+    profile = "default"
+  
+}
+
+resource "aws_instance" "name" {
+  ami = "ami-0ac7b260cf76d8865"
+  instance_type = "t3.small"
+
+   tags = {
+    Name = "my-terra"
+  }
+  
+}
